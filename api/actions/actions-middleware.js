59ad45fff4720975bfa2dc53dce0ba56c83fa042
Ethan Edmond <eedmond@uccs.edu>
2021-07-09 11:57:28 -0600

    Finished action logger

--- api/actions/actions-middleware.js
+++ api/actions/actions-middleware.js
@@ -1,18 +1,21 @@
 // add middlewares here related to actions
 
 function actionLogger(req, res, next) {
-  console.log("----- Action Logger -----");
-//  console.log("Req Method: ", req.method);
+  console.log('----- Action Logger -----');
+  console.log('Req Method: ', req.method);
+  console.log('Req URL: ', req.protocol + '://' + req.get('host'));
+  console.log("TimeStamp: ", Date());
+  console.log('-------------------------');
   next();
 }
 
 function validateActionId(req, res, next) {
-  console.log("Validate Action ID");
+  console.log('Validate Action ID');
   next();
 }
 
 function validateAction(req, res, next) {
-  console.log("Validate Action");
+  console.log('Validate Action');
   next();
 }
 
