eee9b460149470032f5499b6084ad028e6d3da84
Ethan Edmond <eedmond@uccs.edu>
2021-07-09 11:17:49 -0600

    Made skeleton for routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,40 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'get on /api/actions'
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'get on /api/actions/:id'
+  });
+
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'post on /api/actions'
+  });
+
+});
+
+router.put('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'put on /api/actions/:id'
+  });
+
+});
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'delete on /api/actions/:id'
+  });
+
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,42 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'get on /api/projects/'
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'get on /api/projects/:id'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'post on /api/projects/'
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'put on /api/projects/:id'
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({
+	  message: 'delete on /api/projects/:id'
+  });
+});
+
+router.get('/:id/actions', (req, res, next) => {
+  res.status(200).json({
+	  message: 'get on /api/projects/:id/actions'
+  });
+});
+
+module.exports = router;
